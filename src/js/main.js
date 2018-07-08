4f27e1de23f7bd3ec3606238e3428e0d2a23df89
Alexander Podgornev <podgornev.alexaner@gmail.com>
2018-07-09 01:24:00 +0300

    Complete variant

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,18 @@
 'use strict';
-$(document).ready(() => {
 
+let is_open = false;
+
+$(document).ready(() => {
+    $("#hamb-menu").click(() => {
+        if(!is_open) {
+            $('#hamb-menu').addClass('open');
+            is_open = true;
+        } else {
+            $('#hamb-menu').removeClass('open');
+            is_open = false;
+        }
+        
+        $('#header-menu').slideToggle(300);
+    });
+     
 });
